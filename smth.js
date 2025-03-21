a794e8b987dd3bcf843001261c42f52dff4a025d
shaparvv <shaparvladyslav21@gmail.com>
2025-03-21 17:49:08 +0200

    Update smth.js

--- smth.js
+++ smth.js
@@ -10,3 +10,13 @@ button.addEventListener('click', () => {
     button.remove(); // Removes the button from the DOM
   }, 2000);
 });
+
+
+window.onscroll = function() { updateProgressBar(); };
+
+function updateProgressBar() {
+    let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    let scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrollPercent = (scrollTop / scrollHeight) * 100;
+    document.querySelector(".progress-bar").style.width = scrollPercent + "%";
+}
