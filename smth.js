b5614947ab0f9f2e24279b168ec35cea54cdeefe
shaparvv <shaparvladyslav21@gmail.com>
2026-09-21 21:55:04 +0300

    Update smth.js

--- smth.js
+++ smth.js
@@ -4,11 +4,19 @@ const audio = document.getElementById('background-music');
 const button = document.getElementById('play-music');
 
 button.addEventListener('click', () => {
-  audio.play();
-  button.style.opacity = '0';
-  setTimeout(() => {
-    button.remove(); // Removes the button from the DOM
-  }, 2000);
+
+    setTimeout(() => {
+
+        audio.play();
+
+        button.style.opacity = '0';
+
+        setTimeout(() => {
+            button.remove();
+        }, 2000);
+
+    }, 1000);
+
 });
 
 
